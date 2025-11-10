114ee4482d0f9b936113f962910121aaab13fc56
Michael Langat <langatkipmichael@gmail.com>
2025-11-10 19:34:45 +0300

    initial commit and project setup

---
+++ src/main/java/com/michaellangat/wera/WeraApplication.java
@@ -0,0 +1,13 @@
+package com.michaellangat.wera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeraApplication.class, args);
+	}
+
+}
